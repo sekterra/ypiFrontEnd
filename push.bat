677300a3f9a520e89880ed69abd9e4be1e5ccf41
sekterra <sekterra@nate.com>
2018-12-14 15:49:00 +0900

    등록, 수정, 삭제 기능 추가

--- push.bat
+++ push.bat
@@ -7,5 +7,5 @@ set /p str=커밋 메시지를 입력하세요:
 if "%str%" == "" %str% = "Push to git at vuetify_upgrade branch for IOS test"
 
 git commit -m "%str%"
-git push origin vuetify_upgrade
+git push origin master
 echo ======= GIT push complete ======
